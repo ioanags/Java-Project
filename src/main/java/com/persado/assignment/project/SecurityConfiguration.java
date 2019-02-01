b6dfdcfbbe148520cb99db81557cb46f642d6a6c
Giannis <giannhsags@gmail.com>
2019-02-01 17:30:23 +0200

    spring-security-configuration-to-not-require-login-and-allow-h2-console

---
+++ src/main/java/com/persado/assignment/project/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.persado.assignment.project;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        //disable login for any resource
+        httpSecurity.authorizeRequests().anyRequest().permitAll();
+
+        //this code will allow h2-console in addition with Spring Security
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+
+    }
+
+}
